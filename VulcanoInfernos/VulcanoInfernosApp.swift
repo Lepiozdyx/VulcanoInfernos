316687da0b1433a1d502008755b615034ab72a04
Developer <Lepiozdyx@gmail.com>
2025-10-22 10:48:31 -0300

    Initial Commit

---
+++ VulcanoInfernos/VulcanoInfernosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulcanoInfernosApp.swift
+//  VulcanoInfernos
+//
+//  Created by Alex on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VulcanoInfernosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
